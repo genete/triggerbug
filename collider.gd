f921387d69dd3006aced36f31475bd85d5018671
Carlos Lopez <genetita@gmail.com>
2016-05-07 00:07:18 +0200

    Comment line 8

--- collider.gd
+++ collider.gd
@@ -5,7 +5,7 @@ signal my_signal
 
 func _ready():
 	var trigger=get_node("/root/Node2D/Trigger")
-	add_user_signal("my_signal")
+#	add_user_signal("my_signal")
 	connect("my_signal", trigger, "_callback_trigger")
 	if has_user_signal("my_signal"):
 		print("Collider has user signal: my_signal")
